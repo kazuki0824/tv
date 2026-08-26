78c5edbe944b63cc2a529c63e7c6b8fe46925788
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-08-27 00:25:49 +0900

    test: cover nullable Binder boundary paths

--- tuner_hal2/aidl_service/src/tuner_service/frontend_methods.rs
+++ tuner_hal2/aidl_service/src/tuner_service/frontend_methods.rs
@@ -53,8 +53,11 @@ fn frontend_readiness_from_query_value(
 }
 
 impl IFrontend for FrontendAidlObject {
-    fn setCallback(&self, callback: &Strong<dyn IFrontendCallback>) -> BinderResult<()> {
-        self.set_callback_nullable_for_aidl(Some(callback))
+    fn setCallback(
+        &self,
+        callback: Option<&Strong<dyn IFrontendCallback>>,
+    ) -> BinderResult<()> {
+        self.set_callback_nullable_for_aidl(callback)
     }
     fn tune(&self, settings: &FrontendSettings) -> BinderResult<()> {
         execute_shared_object_runtime_use_case_with_request_builder(
