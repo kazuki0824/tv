6fe92e55afd106a5c748b83da99f58943c3e316e
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-09-01 09:20:08 -0700

    style: align VTS test integration comments

--- tuner_hal2/config/vts_test_agent_integration.mk
+++ tuner_hal2/config/vts_test_agent_integration.mk
@@ -1,3 +1,3 @@
-# Explicit VTS/test-image integration only.
-# Normal tuner_hal2 product integration intentionally does not inherit this file.
+# 明示的なVTS/test image統合でのみ使用する。
+# 通常のtuner_hal2 product integrationからはこのfileを継承しない。
 PRODUCT_PACKAGES += maleicacid_tuner_hal2_vts_agent
