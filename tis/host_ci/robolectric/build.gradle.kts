578482babb12d5ba3a2b57068f485f1495800144
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-09-12 16:43:12 -0700

    test(tis): compile shared acceptance tests for Robolectric

--- tis/host_ci/robolectric/build.gradle.kts
+++ tis/host_ci/robolectric/build.gradle.kts
@@ -20,11 +20,6 @@ android {
         }
         getByName("test") {
             java.setSrcDirs(listOf("../../tests/src"))
-            java.include(
-                "com/maleicacid/tvinput/tis/DirectBootGuardR51FixTest.kt",
-                "com/maleicacid/tvinput/tis/RecordingDisabledR51Test.kt",
-                "com/maleicacid/tvinput/tis/TvProviderWriterDescriptorSchemaTest.kt",
-            )
         }
     }
 
@@ -40,6 +35,7 @@ kotlin {
 dependencies {
     compileOnly("org.robolectric:android-all:15-robolectric-13954326")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.22")
     testImplementation("androidx.test:core:1.7.0")
     testImplementation("androidx.test.ext:junit:1.3.0")
     testImplementation("org.robolectric:robolectric:4.16.1")
