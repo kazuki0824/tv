2ab4692bc26be99a5c19ad02d7b7443b2485189f
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-09-14 18:35:41 -0700

    fix(tuner): B25固定parameterの配置名を方式別にする

--- tuner_hal2/config/config.fs
+++ tuner_hal2/config/config.fs
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
-# 製品入力の所有者・権限をvendor image作成時に設定する。
-[vendor/etc/maleicacid/multi2_parameters]
+# 製品固定入力の所有者・権限をvendor image作成時に設定する。
+[vendor/etc/maleicacid/b25_multi2_parameters]
 mode: 0640
 user: AID_ROOT
 group: AID_SYSTEM
