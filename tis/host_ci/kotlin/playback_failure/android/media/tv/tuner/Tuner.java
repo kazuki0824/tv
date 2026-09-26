8c0929cece4a92377e97d531c38af8fde0f9a649
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-09-25 21:40:44 -0700

    test(tis): observe production section filter opens

--- tis/host_ci/kotlin/playback_failure/android/media/tv/tuner/Tuner.java
+++ tis/host_ci/kotlin/playback_failure/android/media/tv/tuner/Tuner.java
@@ -8,8 +8,17 @@ import java.util.concurrent.Executor;
 public class Tuner {
     public static final byte[] VOID_KEYTOKEN = new byte[] {0};
     public static Filter nextFilter;
+    public static int openFilterCalls;
+    public static int sectionFilterCount = 16;
+
     public static int getTunerVersion() { return 0x00030000; }
+
+    public DemuxCapabilities getDemuxCapabilities() {
+        return new DemuxCapabilities(sectionFilterCount);
+    }
+
     public Filter openFilter(int type, int subtype, long bufferSize, Executor executor, FilterCallback callback) {
+        openFilterCalls++;
         return nextFilter;
     }
 }
